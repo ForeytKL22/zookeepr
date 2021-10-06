f4738662e17af6a9c08cacc4eeeb54970ba00bca
ForeytKL22 <krforeyt@gmail.com>
2021-10-05 20:57:16 -0600

    lesson 3

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const { animals } = require('./data/animals');
 const fs = require('fs');
 const path = require('path');
-
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extend: true }));
 // parse incoming JSON data
 app.use(express.json());
+
+app.use(express.static('public'));
 
 
 function filterByQuery(query, animalsArray) {
@@ -85,6 +87,22 @@ app.get('/api/animals/:id', (req, res) => {
   } else {
     res.send(404);
   }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.post('/api/animals', (req, res) => {
